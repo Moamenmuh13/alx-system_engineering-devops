b6a52d1b6b1657f0007e7db457f773cb5d63bfe9
Mu'men Muhammad <mumenmuhammad13@gmail.com>
2024-01-22 00:10:11 +0200

    finished

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# 1-install_a_package.pp
+
+# Define a package resource for Flask
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Ensure that python3-pip is installed
+package { 'python3-pip':
+  ensure => present,
 }
